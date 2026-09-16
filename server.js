b95d68478b8f5e9481da14598304feb6d6d956b9
sainaihirwe-cmd <sainaihirwe@gmail.com>
2026-09-16 09:42:04 +0100

    feat:added new features

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
+const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const mongoSanitize = require('express-mongo-sanitize');
 
